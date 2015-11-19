c5f40196acfa27e2e048252a82f6566f504c0de9
Caleb Scholze <caleb.scholze@gmail.com>
2015-11-19 17:05:53 -0600

    Completed project setup

---
+++ scripts/dependencies.js
@@ -0,0 +1,6 @@
+define(function(require) {
+	var $ = require("jquery");
+	var b = require("bootstrap");
+	var h = require("hbs");
+	var Firebase= require("firebase");
+});
---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+requirejs.config({
+	baseURL: "./scripts",
+	paths: {
+		"jquery": "../lib/bower_components/jquery/dist/jquery.min",
+		"hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+		"lodash": "../lib/bower_components/loadsh/lodash.min",
+		"Q": "../lib/bower_components/q/q",
+		"firebase": "../lib/bower_components/firebase/firebase",
+		"bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min"
+	},
+	shim: {
+		"bootstrap": "jquery",
+		"firebase": {
+			exports: "Firebase"
+		}
+	}
+});
+
+require(["dependencies"], function(_$_) {
+	console.log("Hellow from main.js");
+});
