a2b1f414645d5694aaa2401f08278d8384d54f30
Caleb Scholze <caleb.scholze@gmail.com>
2015-11-20 16:26:54 -0600

    displaying errors, and linked initial page after succesful login

--- scripts/login.js
+++ scripts/login.js
@@ -14,33 +14,65 @@ define(["firebase","templates"], function (Firebase, templates) {
 			password : $("#pwd").val()
 			}, function(error, userData) {
 				if (error) {
+				// log error message and error
 				console.log("Error creating user:", error);
+				// display error message and error in DOM
+				$("#error").html("Create New User "+error);
+				// Reset form inputs on fail
+					$("#email").val("");
+					$("#pwd").val("");
 				} else {
+				// log success message and uid for new user
 				console.log("Successfully created user account with uid:", userData.uid);
+				// display success message on DOM
+				$("#error").html("Successfully created user account.");
+				
+				// store user under 'users' location in firebase, but this command
+				// may need to wait until a user adds a movie, then push
+				// {movieref: rating || "unwatched"} to user firebase ref
 				var userID = userData.uid;
-				usersRef.set({userID: "movie refs"});
+				usersRef.child(userID).set({"movieRef": "rating"});
+				
+				// direct user to initial_page after successful login
+				templates.loadInitialPage();
 				} // end else
 			}); // end  onComplete callback for ref.createUser
 		}, // end login.createNewUser
 
 		loginUser: function() {
 			ref.authWithPassword({
-			// get email and password from splash page
-			// call this function on login button click
 			
-			email: "toferdev@yourmom.com",
-			password: "password"
+			/* TEST LOGIN CODE HERE */
+			/* UNCOMMMENT NEXT TWO LINES TO AVOID TYPING IN LOGIN INFO */
+			// email    : "test@test.com",
+			// password : "pass"
+			/* END TEST LOGIN PARAMS */
 
+			/* PRODUCTIN LOGIN CODE */
 			/* UNCOMMMENT NEXT TWO LINES FOR PRODUCTION USE */
-			// email    : $("#email").val(),
-			// password : $("#pwd").val()
-			
+			 email    : $("#email").val(),
+			 password : $("#pwd").val()
+			/* END PRODUCTION LOGIN PARAMS */
+
+
 			}, function(error, authData) {
 				if (error) {
-				console.log("Login Failed!", error);
+					// log login fail error message and error
+					console.log("Login Failed!", error);
+					// display error message and error on dom
+					$("#error").html("Login "+error);
+					
+					// Reset form inputs on fail
+					$("#email").val("");
+					$("#pwd").val("");
+
 				} else {
-				console.log("Authenticated successfully with payload:", authData);
-				console.log("authData.uid", authData.uid);
+					// log success message and user data
+					console.log("Authenticated successfully with payload:", authData);
+					// display success message on DOM
+					$("#error").html("Authenticated successfully.", error);
+					// direct user to initial_page after successful login
+					templates.loadInitialPage();
 				} // end else
 			}); // end onComplete callback for ref.authWithPassword
 		} // end loginUser
--- scripts/templates.js
+++ scripts/templates.js
@@ -4,6 +4,12 @@ define(function (require) {
 			require(["hbs!../templates/splash"], function (throwSplash) {
 				$("#catcher").html(throwSplash);
 			});
+		},
+
+		loadInitialPage: function() {
+			require(["hbs!../templates/initial_page"], function (throwInitialPage) {
+				$("#catcher").html(throwInitialPage);
+			});	
 		}
 	};
 });
