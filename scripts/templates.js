34f51a076b99084e6427c0eb2040388db86b5c9c
Caleb Scholze <caleb.scholze@gmail.com>
2015-11-20 16:36:23 -0600

    linked modals to navbar on homepage

--- scripts/templates.js
+++ scripts/templates.js
@@ -9,7 +9,13 @@ define(function (require) {
 		loadInitialPage: function() {
 			require(["hbs!../templates/initial_page"], function (throwInitialPage) {
 				$("#catcher").html(throwInitialPage);
-			});	
+			});
+			require(["hbs!../templates/find_movies"], function (throwFindMoviesModal) {
+				$("#find-movies-modal").html(throwFindMoviesModal);
+			});
+			require(["hbs!../templates/search_movies"], function (throwSearchMoviesModal) {
+				$("#search-movies-modal").html(throwSearchMoviesModal);
+			});
 		}
 	};
 });
